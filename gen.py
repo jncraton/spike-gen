3b3464c2d87ecfe8d84b5915f7156eb59242681d
Jon Craton <jncraton@gmail.com>
2024-05-02 19:07:40 -0400

    Draw main art

--- gen.py
+++ gen.py
@@ -19,15 +19,19 @@ def draw_card(name, hp):
     # Draw hp
     draw.text((175, 15), f"HP : {hp}", "black")
 
-    draw.rectangle([(10, 30), (215, 150)], fill="white", outline="black", width=1)
-
     # Add the type icon
     type_img = Image.open("media/types/lightning.png")
     img.paste(type_img, (154, 10, 170, 26), type_img)
 
+    # Draw main art border
+    draw.rectangle([(10, 30), (215, 150)], fill="tan", outline="black", width=1)
+
+    # Draw main art
+    spike_img = Image.open("media/spikeyes/britebulb01.png")
+    img.paste(spike_img, (10, 30, 215, 150), spike_img)
+
     # Save the image
     img.save(f"{name}.png")
-
 
 spikeyes = [
 {
